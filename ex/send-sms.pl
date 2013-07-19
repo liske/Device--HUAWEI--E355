8169be28517e2ef0e6a505524c48cb304da62993
Thomas Liske <thomas@fiasko-nw.net>
2013-07-19 13:13:15 +0200

    Fix send_sms usage in example script.

--- ex/send-sms.pl
+++ ex/send-sms.pl
@@ -7,8 +7,18 @@ use Device::HUAWEI::E355;
 
 my $devip = shift || die "Usage: $0 <devip> <msg> <phone>+\n";
 my $msg = shift || die "Usage: $0 <devip> <msg> <phone>+\n";
-my @phones = @_;
-my $e355 = Device::HUAWEI::E355->new($devip);
+die "Usage: $0 <devip> <msg> <phone>+\n" if($#ARGV == -1);
+
+$|++;
+print "Username: ";
+my $uname = <>;
+chomp($uname);
+
+print "Password: ";
+my $passw = <>;
+chomp($passw);
+
+my $e355 = Device::HUAWEI::E355->new($devip, $uname, $passw);
 
 use Data::Dumper;
-print Dumper($e355->send_sms($msg,  @phones));
+print Dumper($e355->send_sms($msg,  @ARGV));
